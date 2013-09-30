370f89bf541b54142f93d8963a0f8b376ccf0dbb
Pepi Dimi <pepi55@live.com>
2013-09-30 12:08:34 +0200

    asteroid spawns smaller enemies

---
+++ Assets/Scripts/DestroyAsteroid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAsteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
